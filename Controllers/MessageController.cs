a71bf4a4021821df85f732bf9b5ad86b76509440
FurkanB190803 <bagfurkan114@gmail.com>
2024-08-29 18:08:52 +0300

    A

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ServerApi.Controllers
 {
@@ -7,7 +8,6 @@ namespace ServerApi.Controllers
     [Route("api/[controller]")]
     public class MessageController : ControllerBase
     {
-        // Basit bir liste ile mesajlarý depola
         private static List<string> _messages = new List<string>();
 
         // GET isteði ile tüm mesajlarý döndür
@@ -22,8 +22,29 @@ namespace ServerApi.Controllers
         public IActionResult Post([FromBody] string message)
         {
             _messages.Add(message);
-            Console.WriteLine(message); // Konsola yazdýr
+            Console.WriteLine(message);
             return Ok("Message received!");
+        }
+
+        // SSE endpoint'i ile mesajlarý stream et
+        [HttpGet("stream")]
+        public async Task Stream()
+        {
+            Response.ContentType = "text/event-stream";
+            Response.Headers.Add("Cache-Control", "no-cache");
+            Response.Headers.Add("Connection", "keep-alive");
+
+            while (true)
+            {
+                if (Response.HttpContext.RequestAborted.IsCancellationRequested)
+                    break;
+
+                var message = string.Join("\n", _messages);
+                await Response.WriteAsync($"data: {message}\n\n");
+                await Response.Body.FlushAsync();
+
+                await Task.Delay(50); // 50 ms gecikme
+            }
         }
     }
 }
