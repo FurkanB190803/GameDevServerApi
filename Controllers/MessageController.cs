b0d34db7a42c68da5b2506059d4cb19ccdae3eec
FurkanB190803 <bagfurkan114@gmail.com>
2024-09-02 18:27:18 +0300

    Update MessageController.cs

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -2,40 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace ServerApi.Controllers
 {
-    
-   /* public class KeyPressModel
-    {
-        public bool IsPressed { get; set; }
-        public string Key { get; set; }
-        public string Timestamp { get; set; }
-    }*/
     public class KeyPressData
     {
         public string keyCode;
         public List<string> pressTimes = new List<string>();
         public List<string> releaseTimes = new List<string>();
     }
+
     public class MouseMovementData
     {
         public string timestamp;
         public float deltaX;
         public float deltaY;
     }
-/*
-    public class CameraDataModel
-    {
-        public string px { get; set; }
-        public string py { get; set; }
-        public string pz { get; set; }
-        public string cx { get; set; }
-        public string cy { get; set; }
-        public string cz { get; set; }
-        public string Timestamp { get; set; }
-    }
-*/
 
     [ApiController]
     [Route("ping/[controller]")]
@@ -48,21 +31,13 @@ namespace ServerApi.Controllers
             return Ok(new { message = "Pong", timestamp = DateTime.UtcNow });
         }
     }
-    
-
 
     [ApiController]
     [Route("api/[controller]")]
     public class MessageController : ControllerBase
     {
-        //private static List<KeyPressModel> _keyPresses = new List<KeyPressModel>();
-
-        //private static List<CameraDataModel> _cameraData = new List<CameraDataModel>();
-
         private static List<KeyPressData> _keyPressData = new List<KeyPressData>();
-
         private static List<MouseMovementData> _mouseMovementData = new List<MouseMovementData>();
-
 
         [HttpPost("KeyInput")]
         public IActionResult PostKeyInput([FromBody] KeyPressData keyPressData)
@@ -73,9 +48,15 @@ namespace ServerApi.Controllers
             }
 
             _keyPressData.Add(keyPressData);
+
+            // Log the received data
+            Console.WriteLine("KeyInput received:");
+            Console.WriteLine($"KeyCode: {keyPressData.keyCode}");
+            Console.WriteLine($"Press Times: {string.Join(", ", keyPressData.pressTimes)}");
+            Console.WriteLine($"Release Times: {string.Join(", ", keyPressData.releaseTimes)}");
+
             return Ok("KeyPress received!");
         }
-
 
         [HttpPost("MouseInput")]
         public IActionResult PostMouseInput([FromBody] MouseMovementData mouseMovementData)
@@ -86,74 +67,44 @@ namespace ServerApi.Controllers
             }
 
             _mouseMovementData.Add(mouseMovementData);
-            return Ok("KeyPress received!");
+
+            // Log the received data
+            Console.WriteLine("MouseInput received:");
+            Console.WriteLine($"Timestamp: {mouseMovementData.timestamp}");
+            Console.WriteLine($"DeltaX: {mouseMovementData.deltaX}");
+            Console.WriteLine($"DeltaY: {mouseMovementData.deltaY}");
+
+            return Ok("MouseInput received!");
         }
 
         [HttpGet("getKeyInput")]
         public IActionResult GetKeyInput()
         {
+            // Log the data being sent
+            Console.WriteLine("Getting KeyInput:");
+            foreach (var data in _keyPressData)
+            {
+                Console.WriteLine($"KeyCode: {data.keyCode}");
+                Console.WriteLine($"Press Times: {string.Join(", ", data.pressTimes)}");
+                Console.WriteLine($"Release Times: {string.Join(", ", data.releaseTimes)}");
+            }
+
             return Ok(_keyPressData);
         }
+
         [HttpGet("getMouseInput")]
         public IActionResult GetMouseInput()
         {
+            // Log the data being sent
+            Console.WriteLine("Getting MouseInput:");
+            foreach (var data in _mouseMovementData)
+            {
+                Console.WriteLine($"Timestamp: {data.timestamp}");
+                Console.WriteLine($"DeltaX: {data.deltaX}");
+                Console.WriteLine($"DeltaY: {data.deltaY}");
+            }
+
             return Ok(_mouseMovementData);
         }
-
-
-
-
-
-
-
-
-        /*
-
-        // POST: api/message/keypress
-
-        [HttpPost("keypress")]
-        public IActionResult PostKeyPress([FromBody] KeyPressModel keyPress)
-        {
-            if (keyPress == null)
-            {
-                return BadRequest("Invalid input");
-            }
-
-            _keyPresses.Add(keyPress);
-            return Ok("KeyPress received!");
-        }
-
-        // GET: api/message/keypresses
-        [HttpGet("keypresses")]
-        public IActionResult GetKeyPresses()
-        {
-            return Ok(_keyPresses);
-        }
-
-
-      
-        [HttpGet("cameras")]
-        public IActionResult GetCamera()
-        {
-            return Ok(_cameraData);
-        }
-
-        // POST: api/message/camera
-
-       
-
-        [HttpPost("camera")]
-        public IActionResult PostCameraData([FromBody] CameraDataModel cameraData)
-        {
-            if (cameraData == null)
-            {
-                return BadRequest("Geçersiz giriþ");
-            }
-
-            _cameraData.Add(cameraData);
-            return Ok("Kamera verisi alýndý!");
-        }*/
     }
-
-
 }
